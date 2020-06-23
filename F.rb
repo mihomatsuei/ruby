aa9b5bf58e0ad9ca8a3dc30316a3aeaad18614c9
松榮美帆 <>
2020-06-24 01:52:26 +0900

    [Start]2-8

---
+++ F.rb
@@ -0,0 +1,18 @@
+def hello_world
+	"Hello,World!"
+end
+
+puts hello_world
+
+def greeting(name)
+	"Hello, #{name}!"
+end
+
+puts greeting("John")
+
+def greeting
+	return "Hello,Jhon!"
+	"Hi,Jhon!"
+end
+
+puts greeting
